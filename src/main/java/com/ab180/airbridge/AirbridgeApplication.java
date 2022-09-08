b8f1400dbfdc5186ac391153e592d33fbabd0a3e
Anjeongkyun <dkswjdrbs12@naver.com>
2022-09-08 19:27:58 +0900

    first commit

---
+++ src/main/java/com/ab180/airbridge/AirbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.ab180.airbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ab180/airbridge/AirbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab180.airbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
